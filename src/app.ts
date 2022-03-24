2de6e4d6a36162a02e05e114651a363efc82d346
KHUNBIRD <birdpakornpong@gmail.com>
2022-03-24 13:20:55 +0700

    add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express from "express";
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs'
+
 const app = express();
 const port = 3000;
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
-
-  
   res.send("Hello World!");
+});
+
+app.get("/test", (req, res) => {
+  res.send("Test!");
 });
 
 app.listen(port, () => {
